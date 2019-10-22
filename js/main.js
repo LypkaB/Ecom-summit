7d04d41d299e2006af7a890035d42282f26e367a
LypkaB <lpk.bdn@gmail.com>
2019-10-22 12:05:10 +0300

    Created pop-up window

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ $('.banner__video-btn-play').click(function() {
     $('.banner__video-btn-play, .banner__video img').css('display', 'none');
     $('.banner__video-player').css('display', 'block');
 });
+
+/*<----- Pop-up window ----->*/
+$('.header__list li:last-child, .btn-get, .sidebar a').on('click', function () {
+    $('.overlay').show();
+});
+
+$('.btn-close').on('click', function () {
+    $('.overlay').hide();
+});
