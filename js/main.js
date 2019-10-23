1250608ab16260b0e3afc8d36456493b3401e2e2
LypkaB <lpk.bdn@gmail.com>
2019-10-23 14:26:57 +0300

    Added navigation for nav bar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-/*<----- Play video ----->*/
-$('.banner__video-btn-play').click(function() {
-    $('.banner__video img, .banner__video-btn-play').css('display', 'none');
-    $('.banner__video-player').css('display', 'block');
-});
+$(function () {
+    /*<----- Play video ----->*/
+    $('.banner__video-btn-play').click(function() {
+        $('.banner__video img, .banner__video-btn-play').css('display', 'none');
+        $('.banner__video-player').css('display', 'block');
+    });
 
-/*<----- Pop-up window ----->*/
-$('.header__list li:last-child, .btn-get, .sidebar a').on('click', function () {
-    $('.overlay').show();
-});
+    /*<----- Pop-up window ----->*/
+    $('.header__list li:last-child, .btn-get, .sidebar a').on('click', function () {
+        $('.overlay').show();
+    });
 
-$('.btn-close').on('click', function () {
-    $('.overlay').hide();
+    $('.btn-close').on('click', function () {
+        $('.overlay').hide();
+    });
+
+    /*<----- Navigation menu ----->*/
+    $('.header__btn-menu').on('click', function() {
+        $('.header__list').slideToggle();
+    });
 });
