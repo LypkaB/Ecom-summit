bc8d3c606a6b2b6933ba34b4b137477fe9e259f8
LypkaB <lpk.bdn@gmail.com>
2020-02-15 11:59:01 +0200

    Fixed some bugs

---
+++ learn/index.php
@@ -0,0 +1,4 @@
+<?php
+ header('HTTP/1.1 301 Moved Permanently');
+ header('Location: https://m.me/vancesoftwares?ref=w7939501');
+ exit();
--- privacy/index.php
+++ privacy/index.php
@@ -1,5 +1,4 @@
-<?php 
- header("HTTP/1.1 301 Moved Permanently"); 
- header("Location: https://app.termly.io/document/privacy-policy/4d3c2f2c-4dcd-4676-afbe-81501d5f3735"); 
- exit(); 
- ?> 
+<?php
+ header('HTTP/1.1 301 Moved Permanently');
+ header('Location: https://app.termly.io/document/privacy-policy/4d3c2f2c-4dcd-4676-afbe-81501d5f3735');
+ exit();
